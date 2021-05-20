311d3f1fd0e68191f388c5848ec228e9b6c96db1
BEAST GLATISANT <26509147+g147@users.noreply.github.com>
2021-05-20 14:08:59 +0530

    updated

--- gen_readme.py
+++ gen_readme.py
@@ -1,19 +1,21 @@
-import os, plazy
+import os
+import plazy
 path = plazy.list_files(os.getcwd())
 templates = []
 for file in path:
     if ".yaml" in file:
         templates.append(file)
-markdown = "| TEMPLATE | TOOL | FILE |\r"
-markdown += "| :----: | :----: | :----: |\r"
 templates.sort()
+markdown = "# Kenzer Templates [{0}]\r".format(len(templates))
+markdown += "| TEMPLATE | TOOL | FILE |\r"
+markdown += "| :----: | :----: | :----: |\r"
 for template in templates:
     tool = template.split("\\")[1].split(".")[0]
     sign = template.split("\\")[-1].split(".")[0]
-    path = "https://github.com/ARPSyndicate/kenzer-templates/tree/main"+template.replace("\\","/")
-    markdown += "| {0} | {1} | [{2}]({3}) |\r".format(sign, tool, template.strip("\\"), path)
+    path = "https://github.com/ARPSyndicate/kenzer-templates/tree/main" + \
+        template.replace("\\", "/")
+    markdown += "| {0} | {1} | [{2}]({3}) |\r".format(sign,
+                                                      tool, template.strip("\\"), path)
 
 with open("README.md", "w") as f:
     f.write(markdown)
-            
-        
