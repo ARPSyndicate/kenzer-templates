6c6adfb5cabd84421281cbc2f9da3523fbfc56bd
g147 <26509147+g147@users.noreply.github.com>
2021-10-06 13:02:17 +0530

    Wednesday, 06-Oct-21 07:31:57 UTC

--- gen_readme.py
+++ gen_readme.py
@@ -10,12 +10,12 @@ markdown = "# Kenzer Templates [{0}]\r".format(len(templates))
 markdown += "| TEMPLATE | TOOL | FILE |\r"
 markdown += "| :----: | :----: | :----: |\r"
 for template in templates:
-    tool = template.split("\\")[1].split(".")[0]
-    sign = template.split("\\")[-1].split(".")[0]
-    path = "https://github.com/ARPSyndicate/kenzer-templates/tree/main" + \
+    tool = template.split("/")[0].split(".")[0]
+    sign = template.split("/")[-1].split(".")[0]
+    path = "https://github.com/ARPSyndicate/kenzer-templates/tree/main/" + \
         template.replace("\\", "/")
     markdown += "| {0} | {1} | [{2}]({3}) |\r".format(sign,
-                                                      tool, template.strip("\\"), path)
+                                                      tool, template, path)
 
 with open("README.md", "w") as f:
     f.write(markdown)
