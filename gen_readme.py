ad8d6c51c3a6a94a16888446cb02220646e8440e
glatisant <26509147+g147@users.noreply.github.com>
2022-07-01 04:26:59 +0530

    Fri  1 Jul 04:26:59 IST 2022

--- gen_readme.py
+++ gen_readme.py
@@ -3,7 +3,7 @@ import plazy
 path = plazy.list_files(os.getcwd())
 templates = []
 for file in path:
-    if ".yaml" in file:
+    if ".yaml" in file or "exploit.sh" in file:
         templates.append(file)
 templates.sort()
 markdown = "# Kenzer Templates [{0}]\r".format(len(templates))
@@ -11,7 +11,10 @@ markdown += "| TEMPLATE | TOOL | FILE |\r"
 markdown += "| :----: | :----: | :----: |\r"
 for template in templates:
     tool = template.split("/")[0].split(".")[0]
-    sign = ".".join(template.split("/")[-1].split(".")[0:-1])
+    if ".yaml" in template:
+        sign = ".".join(template.split("/")[-1].split(".")[0:-1])
+    else:
+        sign = template.split("/")[-2]
     path = "https://github.com/ARPSyndicate/kenzer-templates/tree/master/" + \
         template.replace("\\", "/")
     markdown += "| {0} | {1} | [{2}]({3}) |\r".format(sign,
