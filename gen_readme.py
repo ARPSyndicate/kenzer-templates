26127dcc8c4d579fca9d062f35b95b859de4c1b9
glatisant <26509147+g147@users.noreply.github.com>
2022-07-02 06:58:14 +0530

    Sat  2 Jul 06:58:14 IST 2022

--- gen_readme.py
+++ gen_readme.py
@@ -3,7 +3,7 @@ import plazy
 path = plazy.list_files(os.getcwd())
 templates = []
 for file in path:
-    if ".yaml" in file or "exploit.sh" in file:
+    if (".yaml" in file or "exploit.sh" in file) and ("freakerdb.yaml" not in file):
         templates.append(file)
 templates.sort()
 markdown = "# Kenzer Templates [{0}]\r".format(len(templates))
@@ -11,7 +11,7 @@ markdown += "| TEMPLATE | TOOL | FILE |\r"
 markdown += "| :----: | :----: | :----: |\r"
 for template in templates:
     tool = template.split("/")[0].split(".")[0]
-    if ".yaml" in template:
+    if ".yaml" in template and "/fingerprints/" not in template:
         sign = ".".join(template.split("/")[-1].split(".")[0:-1])
     else:
         sign = template.split("/")[-2]
